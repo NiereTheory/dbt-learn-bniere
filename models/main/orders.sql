646290e31512a34286c781e16840433a8edbdd86
Ben Niere <bniere@gmail.com>
2020-04-29 16:41:02 +0000

    matching core git project

--- models/main/orders.sql
+++ models/main/orders.sql
@@ -1,8 +1,34 @@
-{{ config(materialized='table') }}
-select 
-    o.order_id
-    , o.customer_id
-    , p.amount
-from stg_orders o
-inner join raw.stripe.payment p
-    on o.order_id = p."orderID"
+{{ 
+    config(materialized='table') 
+}}
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
+),
+
+payments as (
+    select * from {{ ref('stg_payments') }}
+),
+
+order_payments as (
+    select
+        order_id,
+        sum(amount) as amount
+
+    from payments
+    group by 1
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        orders.status,
+        coalesce(order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
+)
+
+select * from final
